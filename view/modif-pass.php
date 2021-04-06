203ebece08c4d74e3229073a566dca1cdbe0f124
fox120773 <81371000+fox120773@users.noreply.github.com>
2021-04-06 16:13:29 +0200

    Update modif-pass.php

--- view/modif-pass.php
+++ view/modif-pass.php
@@ -4,8 +4,6 @@ session_start();
 //Connexion à la base de données
 require('../src/bdd-connect.php');
 
-
-
 //Je teste si je clique sur le bouton envoi
 if (isset($_POST['clic_envoi'])) {
 
@@ -55,16 +53,12 @@ if (isset($_POST['clic_envoi'])) {
           <section>
                <div id="titre-connexion"><h2>Modifier votre mot de passe</h2></div>
 
-              
-               <div id="formulaire"> 
+                <div id="formulaire"> 
                       
                       <div id="message-erreur"> <?php if (isset($erreur)) { echo $erreur;} ?> </div>
                       
                       <form method="post" >
-
                             <table>
-
-
                                 <tr>
                                     <td class="alignement-table-formulaire"><label for="new_pass" >Nouveau mot de passe :</label></td>
                                     <td><input type="password" name="new_pass" id="new_pass"></td>
@@ -77,17 +71,11 @@ if (isset($_POST['clic_envoi'])) {
                                     <td class="alignement-table-formulaire"git add ></td>
                                     <td><input type="submit" value="Modifier le mot de passe"  name="clic_envoi" id="clic_envoi"></td>
                                 </tr>
-
                             </table>
-
-                    </form> 
+                      </form> 
                 </div> 
 
-
-
-
-
-            </section>
+          </section>
             
             <?php include '../inc/footer.php'; ?>
 
